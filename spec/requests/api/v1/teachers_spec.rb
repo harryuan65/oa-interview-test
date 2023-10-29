9246aaaa78d57de2ee5636d08874876331ef9f27
harryuan65 <harryuan.65@gmail.com>
2023-10-30 01:13:24 +0800

    test: follow student api

--- spec/requests/api/v1/teachers_spec.rb
+++ spec/requests/api/v1/teachers_spec.rb
@@ -38,4 +38,47 @@ RSpec.describe "Api::V1::Teachers", type: :request do
       end
     end
   end
+
+  describe "POST /api/v1/teachers/:id/followed_students/:student_id" do
+    context "when follow ship does not exist" do
+      let(:teacher) { create(:teacher) }
+      let(:student) { create(:student) }
+
+      before { post("/api/v1/teachers/#{teacher.id}/followed_students/#{student.id}") }
+
+      it "returns a created response" do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context "when follow ship exists" do
+      let(:ship) { create(:teacher_student_follow_ship) }
+
+      before { post("/api/v1/teachers/#{ship.teacher_id}/followed_students/#{ship.student_id}") }
+
+      it "returns a conflict response" do
+        expect(response).to have_http_status(409)
+      end
+    end
+
+    context "when the teacher does not exists" do
+      let(:student) { create(:student) }
+
+      before { post("/api/v1/teachers/123456/followed_students/#{student.id}") }
+
+      it "returns a not found response" do
+        expect(response).to have_http_status(404)
+      end
+    end
+
+    context "when the student does not exists" do
+      let(:teacher) { create(:teacher) }
+
+      before { post("/api/v1/teachers/#{teacher.id}/followed_students/123456") }
+
+      it "returns a not found response" do
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
 end
--- spec/routing/api/v1/teachers_spec.rb
+++ spec/routing/api/v1/teachers_spec.rb
@@ -8,5 +8,10 @@ describe Api::V1::TeachersController do
       expect(get: "/api/v1/teachers/#{teacher_id}/followed_students").to route_to("api/v1/teachers#list_followed_students",
                                                                                   id: teacher_id)
     end
+
+    it "routes to #follow_student" do
+      expect(post: "/api/v1/teachers/#{teacher_id}/followed_students").to route_to("api/v1/teachers#follow_student",
+                                                                                   id: teacher_id)
+    end
   end
 end
